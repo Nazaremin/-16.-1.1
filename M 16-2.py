81d7a75fa196b41dea57b9dbb06a7791cd9ddbfa
Nazaremin <107619353+Nazaremin@users.noreply.github.com>
2024-12-12 16:02:00 +0300

    Update M 16-2.py

--- M 16-2.py
+++ M 16-2.py
@@ -1,7 +1,6 @@
+from typing import Annotated
 from fastapi import FastAPI, Path
 from fastapi.responses import HTMLResponse
-from fastapi import Query
-from typing import Annotated
 
 app = FastAPI()
 
@@ -15,13 +14,22 @@ def read_admin():
 
 @app.get("/user/{user_id}")
 def read_user(
-    user_id: Annotated[int, Path(gt=0, le=100, description="Enter User ID")]
+    user_id: Annotated[
+        int,
+        Path(title="Enter User ID", ge=1, le=100, example=1)
+    ]
 ):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 @app.get("/user/{username}/{age}")
 def read_user_info(
-    username: Annotated[str, Query(min_length=5, max_length=20, description="Enter username")],
-    age: Annotated[int, Query(ge=18, le=120, description="Enter age")]
+    username: Annotated[
+        str,
+        Path(title="Enter username", min_length=5, max_length=20, example="UrbanUser")
+    ],
+    age: Annotated[
+        int,
+        Path(title="Enter age", ge=18, le=120, example=24)
+    ]
 ):
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
